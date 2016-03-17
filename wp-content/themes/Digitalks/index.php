ab3dc54441fc5edf4f1b8bcd489ce58b702fe04b
davidalr <david.cadc@gmail.com>
2016-03-17 15:31:04 -0500

    inicio maquetcion

---
+++ wp-content/themes/Digitalks/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css">
+    <title>DIGITALKS</title>
+</head>
+<body>
+   <Header>
+       <h1>DIGITALKS</h1>
+       <h2>Entrenamientos Digitales 100% Efectivos</h2>
+       <h3>CURSO DE MARKETING DIGITAL DONDE APRENDERÁS</h3>
+         <div class="container">
+             <figure>
+                 <img src="assets/imagen.png"/>
+             </figure>
+              <div class="list">
+                  <ul>
+                      <li>Desarrollo de estrategias digitales</li>
+                      <li>Google AdWords</li>
+                      <li>Publicidad en Facebook y Twitter</li>
+                      <li>Publicidad en Instagram y Youtube</li>
+                      <li>Seguimiento y Optimización de campañas </li>
+                      <li>Email Marketing</li>
+
+
+                  </ul>
+              </div>
+              <div class="form">
+                  <a href="tel:5715521313">(1)552-1313</a><br>
+                  <span>Registrate y un asesor se pondrá en contacto para brindarte más infrmación</span><br>
+                  <input type="text " name="Nombre*"><br>
+                  <input type="text " name="Apellido*"><br>
+                  <input type="email " name="mail*"><br>
+                  <input type="submit" value="Enviar" />
+
+              </div>
+
+
+         </div>
+   </Header>
+   <section class="marketing">
+       <figure>
+           <img src="assets/imagen.png"/>
+       </figure>
+       <h3><span>LA IMPORTANCIA DE SABER</span>MARQUETING DIGITAL</h3>
+       <p>Formate para nuevas oportunidades laborales.donde los trabajos más requridos están el mundo digital, pues bienvenido.Digitalks Training es una institución online y offline dedicada a la enseñanza del marqueting,la comunicación y la tecnología digital.donde podrás obtener entrenamientos especializados</p>
+       <p>En Digitalks Training,no tenemos pre-requisitos</p>
+
+   </section>
+   <section class="training">
+       BENEFICIOS DE NUESTROS<br>ENTRENAMIENTOS
+       <article>
+           Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+       </article>
+       <article>
+           Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+       </article>
+       <article>
+           Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+       </article>
+       <article>
+           Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+       </article>
+   </section>
+   <section class="issues">
+       TEMARIO DE LO QUE APRENDERAS
+   </section>
+   <section class="testimonials">
+        <strong>TESTIMONIO</strong>
+
+       <figure>
+           <img src="assets/imagen.png"/>
+       </figure>
+       <div>
+           <strong>JUAN RAMOS(FOTOGRAFO-GAY)</strong>
+           <P>La experiencia de los profesores en el medio digital me dan tranquilidad de saber que aprendi con excelentes profesionales.que me ayudan con cualquier duda que tengo para mi negocio</P>
+       </div>
+   </section>
+   <section class="buy">
+    hhhhh
+   </section>
+</body>
+</html>
